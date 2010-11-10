eb186bbb4579b5bdb269f5de4d3e2087338c6bbc
Fernando Lopes <fernandolopes.s@gmail.com>
2010-11-10 13:32:18 -0300

    modificações nos users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates_presence_of :login, :password, :name, :email, :message => 'é necessário'
+  validates_length_of :login, :in => 5..100, :message => 'so pode ter entre 5 e 100 caracteres'
+  validates_length_of :password, :in => 5..50, :message => 'so pode ter entre 5 e 50 caracteres'
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :message => 'inválido'
+  validates_uniqueness_of :login, :message => 'o campo login deve ser único'
+  validates_uniqueness_of :email, :message => 'o campo email deve ser único'
 end
